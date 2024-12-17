3a3b25e2ca6896a4f307765aed27d2ddcf394442
zul-grl <zulgerel06n@gmail.com>
2024-12-17 08:56:02 +0800

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,34 @@
+import { Fragment, useState } from "react";
 import "./app.css";
 
 function Card() {
+  const [bgColor, setBgColor] = useState("red");
+
+  const ymarNegenYmHewle = () => {
+    setBgColor("blue");
+  };
+
   return (
     <div>
       <div>
         <h2>Naruto</h2>
       </div>
       <div className="card">Card</div>
-      <img src="https://cdn.staticneo.com/w/naruto/Nprofile2.jpg" alt="" />
+      <img
+        style={{ backgroundColor: bgColor, padding: 10 }}
+        src="https://cdn.staticneo.com/w/naruto/Nprofile2.jpg"
+        alt=""
+      />
+      <button
+        style={{ padding: "10px 20px" }}
+        onClick={() => ymarNegenYmHewle()}
+      >
+        Click here!
+      </button>
     </div>
   );
 }
+
 function Header() {
   return <div className="header">Header</div>;
 }
